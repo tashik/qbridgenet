e81a3b0791943012fc37bd65d74009ee22a9a0c5
tashik <patrinat@gmail.com>
2024-12-14 15:32:30 +0300

    code generation upgrade

--- QuikOptionDeskApp/BaseAsset.cs
+++ QuikOptionDeskApp/BaseAsset.cs
@@ -3,8 +3,13 @@ namespace QuikOptionDeskApp;
 public class BaseAsset
 {
     public string AssetClassCode { get; set; } = "";
+    public string NameHead { get; set; } = "";
+    
+    public bool IsSpotBased { get; set; }
     public string AssetSecCode { get; set; } = "";
     public int NumStrikes { get; set; } = 5;
     public decimal StrikeStep { get; set; }
+    
+    public int Digits { get; set; }
     public List<DateTime> OptionSeries { get; set; } = new();
 }
--- QuikOptionDeskApp/Program.cs
+++ QuikOptionDeskApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Collections.Concurrent;
+using System.Globalization;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using QuikBridgeNet;
@@ -8,6 +9,8 @@ using Serilog;
 class Program
 {
     public static List<decimal> SubscribedStrikes { get; set; } = new();
+
+    public static ConcurrentDictionary<string, Guid> _subsriptions = new();
     
     static async Task Main(string[] args)
     {
@@ -50,22 +53,21 @@ class Program
 
         decimal centralStrike = 0m;
         
-        globalEventAggregator.SubscribeToInstrumentParameterUpdate( eventArgs =>
+        globalEventAggregator.SubscribeToInstrumentParameterUpdate( async eventArgs =>
         {
             if (eventArgs is { ClassCode: "SPBFUT", ParamName: "LAST" })
             {
                 var asset = baseAssets.FirstOrDefault(a => a.AssetSecCode == eventArgs.SecCode);
                 if (asset == null)
                 {
-                    return Task.CompletedTask;
+                    return;
                 } 
                 if (centralStrike == 0 || Math.Abs(centralStrike - decimal.Parse(eventArgs.ParamValue!, CultureInfo.InvariantCulture)) > asset.StrikeStep / 2)
                 {
-                    return await UpdateCentralStrikeAsync();
+                    await UpdateCentralStrikeAsync(asset, centralStrike);
                 }
             }
             Log.Information("Instrument parameter {Name} current value {Val} ", eventArgs.ParamName, eventArgs.ParamValue);
-            return Task.CompletedTask;
         });
     }
 
@@ -78,5 +80,110 @@ class Program
             decimal strike = centralStrike + (i * asset.StrikeStep);
             newStrikes.Add(strike);
         }
+        
+        var strikesToUnsubscribe = SubscribedStrikes.Except(newStrikes).ToList();
+        var strikesToSubscribe = newStrikes.Except(SubscribedStrikes).ToList();
+
+        foreach (var strike in strikesToSubscribe)
+        {
+            
+        }
+    }
+    
+    public static string GetOptionCode(DateTime series, BaseAsset baseAsset, decimal strike, bool isCall)
+    {
+        var optionCode = baseAsset.NameHead;
+        var optionCodeTail = GetOptionCodeTail(series, optionCode, isCall);
+        var codeLetter = baseAsset.IsSpotBased ? 'C' : 'B';
+        
+        var strikeStr = strike.ToString("F" + baseAsset.Digits, CultureInfo.InvariantCulture).Replace(',', '.');
+       
+        optionCode += strikeStr + codeLetter + optionCodeTail;
+        return optionCode;
+    }
+
+    private static string GetOptionCodeTail(DateTime series, string optionCode, bool isCall)
+    {
+        char[] callLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
+        char[] putLetters = ['M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'];
+        char[] weekLetters = ['A', 'B', 'C', 'D', 'E'];
+
+        var strYear = (series.Year % 10).ToString();
+        var strMonth = isCall ? callLetters[series.Month - 1] : putLetters[series.Month - 1];
+        var week = "";
+        var numExpWeek = NumberWeekOfMonth(series);
+        var totalWeeksInMonth = TotalWeeksInMonth(series);
+
+        switch (numExpWeek)
+        {
+            case 1: week = "A"; break;
+            case 2: week = "B"; break;
+            case 3: 
+                if (optionCode.StartsWith("BR") && (int)series.DayOfWeek == 4)
+                {
+                    week = "C";
+                } else
+                {
+                    week = "";
+                }
+                break;
+            case 4:
+                if (totalWeeksInMonth == 4 && (int) series.DayOfWeek != 4 && optionCode.StartsWith("BR"))
+                {
+                    break;
+                }
+                week = "D";
+                break;
+            case 5:
+                if ( (int)series.DayOfWeek != 4 && optionCode.StartsWith("BR"))
+                {
+                    break;
+                }
+                week = "E";
+                break;
+            default: 
+                week = "";
+                break;
+        }
+
+        return strMonth + strYear + week;
+    }
+    
+    public static int TotalWeeksInMonth(DateTime currentDate)
+    {
+        int daysInMonth = DateTime.DaysInMonth(currentDate.Year, currentDate.Month);
+        DateTime firstOfMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
+        //days of week starts by default as Sunday = 0
+        int firstDayOfMonth = (int)firstOfMonth.DayOfWeek;
+        int weeksInMonth = (int)Math.Floor((firstDayOfMonth + daysInMonth) / 7.0);
+        return weeksInMonth;
+    }
+
+    public static bool IsLastWeekInMonth(DateTime currentDate)
+    {
+        int weeksInMonth = TotalWeeksInMonth(currentDate);
+
+        return weeksInMonth == 4;
+    }
+
+    public static int NumberWeekOfMonth(DateTime currentDate)
+    {
+        int numberWeekOfTheMonth = 0;
+        try
+        {
+            var firstDayOfTheMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
+            var numberDayOfWeek = Convert.ToInt16(firstDayOfTheMonth.DayOfWeek.ToString("D"));
+            numberWeekOfTheMonth = (currentDate.Day + numberDayOfWeek - 2) / 7 + 1;
+
+            if (numberDayOfWeek == 5 || numberDayOfWeek == 6 || numberDayOfWeek == 7)
+            {
+                numberWeekOfTheMonth -= 1;
+            }
+        }
+        catch (Exception ex)
+        {
+            // add to log
+        }
+        return numberWeekOfTheMonth;
     }
 }
